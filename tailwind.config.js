c3450fa5f36ff5bf6c65257ffa6c49a5a1b2a5bf
Arnaud Ligny <arnaud@ligny.org>
2024-02-07 01:02:21 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
       colors: {
         primary: colors.sky,
         neutral: colors.slate,
+        social: {
+          twitter: '#1da1f2',
+          github: '#333',
+          linkedin: '#0077b5',
+          instagram: '#c13584',
+          youtube: '#ff0000',
+        },
       },
     },
   },
